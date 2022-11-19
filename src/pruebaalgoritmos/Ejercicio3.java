4702a8f2328618f5d435d19ea9aeaf2d231ee4c3
cardenas14 <74878984+cardenas14@users.noreply.github.com>
2022-11-18 21:42:57 -0500

    Primer commit

--- src/pruebaalgoritmos/Ejercicio3.java
+++ src/pruebaalgoritmos/Ejercicio3.java
@@ -1,5 +1,6 @@
 package pruebaalgoritmos;
 import java.util.Scanner;
+
 public class Ejercicio3 {
     public static void main(String[] args) {
         int n1, n2, n3, cant;
@@ -8,38 +9,45 @@ public class Ejercicio3 {
         cant = sc.nextInt();
 
         for (int i = 1; i <= cant; i++) {
-            System.out.println("Ingrese el numero 1");
+            System.out.print("Ingrese el numero 1: ");
             n1 = sc.nextInt();
-            System.out.println("Ingrese el numero 2");
+            System.out.print("Ingrese el numero 2: ");
             n2 = sc.nextInt();
-            System.out.println("Ingrese el numero 3");
+            System.out.print("Ingrese el numero 3: ");
             n3 = sc.nextInt();
-            if (n1 == n2 || n2 == n1 || n1 == n3) {
-                System.out.println("Por lo menos 2 de 3 numeros son IGUALES");
-                if (n1 < n2) {
-                    if (n1 < n3) {
-                        System.out.println("El numero menor es " + n1);
-                    } else {
-                        System.out.println("El numero menor es:" + n3);
-                    }
+            System.out.println("------------------------------------");
+            if (n1 < n2) {
+                if (n1 < n3) {
+                    System.out.print("El numero menor es: " + n1);
                 } else {
-                    if (n2 < n3) {
-                        System.out.println("El numero menor es:" + n2);
-                    } else {
-                        System.out.println("El numero menor es:" + n3);
-                    }
+                    System.out.print("El numero menor es: " + n3);
                 }
-                
-                if (n1 > n2) {
-                    if (n1 > n3) {
-                        System.out.println("El numero mayor es " + n1);
-                    } else {
-                        System.out.println("El numero mayor es:" + n3);
-                    }
+            } else {
+                if (n2 < n3) {
+                    System.out.print("El numero menor es: " + n2);
                 } else {
-                    if (n2 > n3) {
-                        System.out.println("El numero mayor es:" + n2);
-                    } else {
-                        System.out.println("El numero mayor es:" + n3);
-                    }
-}}}}}
+                    System.out.print("El numero menor es: " + n3);
+                }
+            }
+            System.out.println("");
+            if (n1 > n2) {
+                if (n1 > n3) {
+                    System.out.println("El numero mayor es: " + n1);
+                } else {
+                    System.out.println("El numero mayor es: " + n3);
+                }
+            } else {
+                if (n2 > n3) {
+                    System.out.println("El numero mayor es: " + n2);
+                } else {
+                    System.out.println("El numero mayor es: " + n3);
+                }
+            }
+            
+            if (n1 == n2 || n2 == n3 || n1 == n3) {
+                System.out.println("Por lo menos 2 de 3 numeros son IGUALES");
+            }else{
+                System.out.println("Ninguno");
+            }
+            System.out.println("------------------------------------");
+}}}
